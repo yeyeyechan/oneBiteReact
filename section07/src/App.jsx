f336132ae61fa7f8af3398291636f51b47559a4d
yeyeyechan <xamevh123@gmail.com>
2025-04-04 11:13:10 +0900

    study

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -4,9 +4,10 @@ import Controller from "./components/Controller";
 import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
   useEffect(() => {
     console.log(count);
-  }, [count]);
+  }, [count, input]);
   const onClickButton = (value) => {
     setCount(count + value);
   };
@@ -14,6 +15,14 @@ function App() {
     <>
       <div className="App">
         <h1>Simple Counter</h1>
+        <section>
+          <input
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+        </section>
         <section>
           {" "}
           <Viewer count={count} />
